9a032e11008d3734c61c5288d3b03b2c859be470
usifiduius <yousanyo2002@icloud.com>
2022-02-22 17:05:05 +0900

    Init: project

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
- res.send('Hello World!');
+ res.json();
 });
+
+console.log()
 
 // app.listen(3000, function () {
 // });
