0106789bb6c1e2ce1568c6e5c0c8ca04872ae8e4
usifiduius <yousanyo2002@icloud.com>
2022-04-09 18:21:19 +0900

    Project

--- api/index.js
+++ api/index.js
@@ -1,28 +1,17 @@
 const express = require('express');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 const app = express();
 const PORT = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
  
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//JS obj to json
-/*const to_json=[{ Name :"Manaya",Gender :"Man"},
-{ Name :"Manuel",Gender :"Man"}]
-const json_string = JSON.stringify(to_json);
-//json to JS obj
-const from_json = JSON.parse(json_string);*/
-
-
-app.post('/', function (req, res){
- console.log(req.body.Name);
-});
-
-//check
-// console.log("Start");
- //console.log(from_json[0].Name);
- //console.log(data);
-app.listen(3000, function () {
-});
+app.post('/', function (req, res) {
+    // console.log(req.body);
+     const data = req.body;
+     console.log('req.body', data);
+     res.send('api: OK');
+    });
+    
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
