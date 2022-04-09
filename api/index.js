3efcb8b28676fb323278f1cd9775c487ca33a1e4
usifiduius <yousanyo2002@icloud.com>
2022-04-09 18:50:49 +0900

    Project

--- api/index.js
+++ api/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-// const res = require('express/lib/response');
+const express = require("express");
+//const request = require("request");
 const app = express();
-const PORT = process.env.PORT || 3000;
-// var bodyParser = require('body-parser');
- 
+const PORT = process.env.PORT || 3000; //Heroku用
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', function (req, res) {
-    // console.log(req.body);
-    const data = req.body["events"][0]["message"]["id"];
-     console.log('req.body', data);
-     res.send('api: OK');
-    });
-    
-process.env.NOW_REGION ? (module.express = app) : app.listen(PORT);
+app.post("/", function (req, res) {
+  //console.log(req.body);
+  const data = req.body["events"][0]["message"]["id"];
+  console.log("req.body", data);
+  res.send("api: ok");
+});
+process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
