2cfffb6c8c50b908e6cd39214eafd5a99685c8ce
usifiduius <yousanyo2002@icloud.com>
2022-04-09 19:29:00 +0900

    Project

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/", function (req, res) {
-  //console.log(req.body);
+  console.log(req.body);
   const data = req.body["events"][0]["message"]["id"];
   console.log("req.body", data);
   res.send("api: ok");
